2474e3801a274729e4fcfed1fe327c6b811d976b
Duy Nguyen <nduytg@gmail.com>
2016-05-07 23:11:57 +0700

    Hàm ls, mode active chưa test

--- ftp_bu.c
+++ ftp_bu.c
@@ -23,7 +23,7 @@
 
 //"96.47.72.72"
 #define PORT_FTP 		21
-#define SERV_ADDR 	"192.168.233.131"
+#define SERV_ADDR 	"192.168.84.130"
 #define BUFSIZE 			1024
 
 #define PASV_PORT 	40000
@@ -33,9 +33,25 @@
 
 char current_dir[PATH_MAX + 1];
 char myIP[16];
+char svIP[16];
+int svPassivePort;
+int mode;		//Mode active: 0, Mode passive: 1
+int dataSock;
 
 int main(int argc, char* argv[])
 {
+	//Kiem tra mode active hay passive
+	if(argc < 2 || (argc >= 2 && (strcmp(argv[1],"-p") == 0)))
+	{
+		mode = 1;
+		printf("Passive mode!\n");
+	}
+	else
+	{
+		mode = 0;
+		printf("Active mode!\n");
+	}
+	
 	int sockfd;
 	struct sockaddr_in server;
 	char buf[BUFSIZE];
@@ -188,10 +204,163 @@ int main(int argc, char* argv[])
     
     printf("Server response: %s", buf);
     memset(buf, 0, sizeof(buf));
+    
+    //Socket o client de nhan data!
+    dataSock = 0;
+    int svPort = 0;
+    
+    if ((dataSock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("Socket can't be created");
+		exit(errno);
+	}
+
+	printf("Data socket was created...\n");
+    
+    //Goi lenh PASSIVE || PORT, tuy theo mode passive hay active
+    if (mode == 1)
+    {
+		//Passive
+		//memset(buf, 0, sizeof(buf));
+		sprintf(buf, "PASV\r\n", pass);
+		//printf("Buffer: %s\n", buf);
+		result = send(sockfd, buf, strlen(buf), 0);
+
+		memset(buf, 0, sizeof(buf));
+		result = recv(sockfd, buf, BUFSIZE, 0);
+		printf("Server response: %s", buf);
+		
+		char *pt = NULL;
+		pt = strtok(buf," ");
+		for(int i=0; i < 4;i++)
+			pt = strtok(NULL,",");
+		
+		svPort = atoi(pt = (strtok(NULL,","))) * 256;
+		svPort = svPort + atoi(pt = strtok(NULL,"\0"));
+		
+		printf("Port received though PORT command return: %d\n",svPort);
+		
+		// Create information for sockaddr_in dest
+		bzero(&server, sizeof(server));
+		server.sin_family = AF_INET;
+		server.sin_port = htons(svPort);
+		
+		printf("Track 1\n");
+		if ( inet_aton(SERV_ADDR, &server.sin_addr.s_addr) == 0 )
+		{
+			perror(SERV_ADDR);
+			exit(errno);
+		}
+
+		// Connect to server
+		printf("Track 2\n");
+		if ( connect(dataSock, (struct sockaddr*)&server, sizeof(server)) != 0 )
+		{
+			perror("Connect can't be established");
+			exit(errno);
+		}
+		printf("Track 3\n");
+		printf("Connected to Server data port!\n");
+		printf("Connection information: \n\tIP address: %s\n\tPort: %d\n\n", SERV_ADDR, svPort);	
+	}
+	else if (mode == 0)
+    {
+		// create new server address to connect
+		 char port[BUFSIZE];
+		 char buff[BUFSIZE];
+		 int min_port = 49153;
+		 int max_port = 65535;
+		struct sockaddr_in serv_addr, client_addr;
+		int sockfd_client, sockfd_server, len, nb, fd, i;
+		//struct hostent *host;
+
+		sockfd_server = socket(AF_INET, SOCK_STREAM, 0);
+		if(sockfd_server < 0)
+		{
+			perror("Socket can't be created!\n");
+			//fclose(f);
+			return 0;
+		}
+
+		//serv_addr.sin_port = htons(20);
+		//inet_aton("192.168.127.2", &serv_addr.sin_addr.s_addr);
+		bzero((char*) &serv_addr, sizeof(serv_addr));
+		serv_addr.sin_family = AF_INET;
+		//serv_addr.sin_addr.s_addr = INADDR_ANY;
+		serv_addr.sin_addr.s_addr = inet_addr(myIP);
+		// memset(&serv_addr.sin_zero, 0, 8);
+
+		len = sizeof(client_addr);
+		int port_num=3425;
+
+		for( i = min_port; i <= max_port; i++)
+		{
+			serv_addr.sin_port = htons(i);
+
+			if(bind(sockfd_server, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) >= 0)
+			{
+				printf("FTP client receive data port: %d\n", i);
+				port_num = i;
+				break;
+			}
+		}
+
+		memset(buff, 0, sizeof(buff));
+		// put interface ip address into buff.
+		sprintf(buff, "%s", myIP);
+		//sprintf(buff, "%s", inet_ntoa(serv_addr.sin_addr));
+
+		for (int j = 0; j <strlen(buff); j++)
+		{
+			if(buff[j] == '.')
+			{
+				buff[j] = ',';
+			}
+		}
+
+		int a = port_num/256;
+		int b = port_num - a*256;
+		sprintf(port, "PORT %s,%d,%d\r\n", buff, a, b);
+		printf("Send command: %s\n", port);
+		if(send(sockfd, port, strlen(port), 0) < 0)
+		{
+			perror("Send error!");
+			//fclose(f);
+			return 0;
+		}
+		//memset(buff, 0, sizeof(buff));
+		if (recv(sockfd, buff, sizeof(buff), 0) < 0)
+		{
+			perror("Receive error!");
+			//fclose(f);
+			return 0;
+		}
+		else
+			printf("Server Response: %s\n", buff);
+
+		sscanf(buff, "%d", &command_code);
+		if(command_code != 200)
+		{
+			parseCode(command_code);
+			return 0;
+		}
+
+		printf("Listen on PORT: %d\n", port_num);
+		int lis = listen(sockfd_server, 5);
+		if(lis < 0)
+		{
+			perror("Can't listen on this socket server");
+			//fclose(f);
+			return 0;
+		}
+	}
+    
+    
     printf ("FTP user command start...\n");
     pwd(sockfd);
     printf("Current Directory: %s\n", current_dir);
     
+    /*
     if(argc == 2)
 		if(strcmp(argv[1], "-p") == 0) // passive mode
 		{
@@ -217,17 +386,99 @@ int main(int argc, char* argv[])
     else // argc = 1
     	goto pasv;
 
+	*/
+	
 	//command_handler(sockfd,"get");
 	command_handler(sockfd,"ls");
+	//put(sockfd, "README.TXT", "/home/nduytg/", 0);
 
     disconnect(sockfd);
-	 freeifaddrs(ifaddr);
+	freeifaddrs(ifaddr);
     memset(buf, 0, sizeof(buf));
     memset(username, 0, sizeof(username));
     memset(pass, 0, sizeof(pass));
     close(sockfd);
 	return 0;
 }
+
+
+//Khoi tao data channel de gui nhan du lieu
+// Dung cho cac ham can goi lenh port len truoc roi moi nhan du lieu: get, put, ls
+int put(int sockfd, char filename[], char localPath[], int mode) // 0 active, 1 passive
+ {
+	char stor[BUFSIZE + 1];
+	int command_code;
+	char buff[BUFSIZE];
+	int len, nb, fd, i;
+	
+	
+	if(strcmp(current_dir, "/") != 0)
+		sprintf(stor, "STOR %s/%s\r\n", current_dir, filename);
+	else
+		sprintf(stor, "STOR %s\r\n", filename);
+
+	char local[PATH_MAX + 1];
+	sprintf(local, "%s/%s", localPath, filename);
+	FILE *f = fopen(local, "rt");
+	
+	if(f == NULL)
+	{
+		printf("Open file error!\n");
+		return 0;
+	}
+
+	printf("\nOpen file: %s\n", local);
+	
+	// send STOR
+	printf("STOR cmd: %s\n",stor);
+	if(send(sockfd, stor, strlen(stor), 0) < 0)
+	{
+		perror("Send STOR error");
+		fclose(f);
+		return 0;
+	}
+	
+	//memset(buff, 0, sizeof(buff));
+	if (recv(sockfd, buff, sizeof(buff), 0) < 0)
+	{
+		perror("Receive error!");
+		fclose(f);
+		return 0;
+	}
+	else
+		printf("Server Response: %s\n", buff);
+
+	sscanf(buff, "%d", &command_code);
+	if(command_code != 200)
+	{
+		parseCode(command_code);
+		return 0;
+	}
+	
+	
+	
+	if(mode == 1) // passive
+	{
+		// send data
+		memset(buff, 0, sizeof(buff));
+		while( (nb = read(f, buff, BUFSIZE))  > 0)
+		{
+			if(send(dataSock, buff, nb, 0) < 0 )
+			{
+				perror("send data error");
+				exit(1);
+			}
+		}
+	}
+	else // active
+	{
+
+	}
+
+	fclose(f);
+	return 1;
+ }
+
 
 void pwd(int sockfd)
 {
@@ -331,7 +582,6 @@ int ftp_recv(int sock, char *arg1, char *arg2)	//RETR
 	return true;
 }
 
-
 void bye(int sock)		//QUIT
 {
 	char buf[100];
@@ -354,7 +604,7 @@ int ls(int sockfd, char *arg1, char *arg2)		//LIST
 	char local_file[256];
 	int file_handler;	
 	char filename[256];
-	//char buf[100];
+	char buf[100];
 	int size;
 
 	if (arg1 == NULL)
@@ -362,143 +612,43 @@ int ls(int sockfd, char *arg1, char *arg2)		//LIST
 	if (arg2 == NULL)
 		arg2 = "~ls.txt";
 	
-	// GOi lenh PORT, tao data channel cho nhan ket qua cua lenh ls
-	// create new server address to connect
-	char port[BUFSIZE];
-	char buff[BUFSIZE];
-	int min_port = 50000;
-	int max_port = 65535;
-	
-	struct sockaddr_in serv_addr, client_addr;
-	int sockfd_client, sockfd_server;
-	int len, fd, i;
-	
-	sockfd_server = socket(AF_INET, SOCK_STREAM, 0);
-	
-	if(sockfd_server < 0)
-	{
-		perror("Socket can't be created!\n");
-		return 0;
-	}
-
-	bzero((char*) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-
-	serv_addr.sin_addr.s_addr = inet_addr(myIP);
-
-	len = sizeof(client_addr);
-	//Neu khong bind duoc trong dong nay thi 40000 cung la 1 port an toan`
-	int port_num = 40000;
-
-	for( i = min_port; i <= max_port; i++)
-	{
-		serv_addr.sin_port = htons(i);
-
-		if(bind(sockfd_server, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) >= 0)
-		{
-			printf("FTP client receive data port: %d\n", i);
-			port_num = i;
-			break;
-		}
-	}
-
-	memset(buff, 0, sizeof(buff));
-	sprintf(buff, "%s", myIP);
-
-	for (int j = 0; j <strlen(buff); j++)
-	{
-		if(buff[j] == '.')
-		{
-			buff[j] = ',';
-		}
-	}
-
-	int a = port_num/256;
-	int b = port_num - a*256;
-	sprintf(port, "PORT %s,%d,%d\r\n", buff, a, b);
-	printf("Send command: %s\n", port);
-	
-	if(send(sockfd, port, strlen(port), 0) < 0)
-	{
-		perror("Send error!");
-		return 0;
-	}
-
-	if (recv(sockfd, buff, sizeof(buff), 0) < 0)
-	{
-		perror("Receive error!");
-		return 0;
-	}
-	else
-	{
-		printf("Server Response: %s\n", buff);
-		int code;
-		sscanf(buff, "%d", &code);
-		if(code != 200)
-		{
-			parseCode(code);
-			return 0;
-		}
-	}
-	
-	strcpy(remote_directory, arg1);
-	strcpy(local_file, arg2);
-
-	memset(buff, 0, sizeof(buff));
-
-	//Tao command LIST de send di
+	//Send LIST command
 	//sprintf(buff, "LIST %s %s\r\n", remote_directory, local_file);
-	sprintf(buff, "LIST");
-	//Send LIST
-	if(send(sockfd, buff, strlen(buff), 0) < 0)
+	sprintf(buf, "LIST\r\n");
+	
+	//printf("Track 1\n");
+	if(send(sockfd, buf, strlen(buf), 0) < 0)
 	{
 		perror("Send LIST error");
 		return 0;
 	}
+	//printf("Track 2\n");
 	
-	printf("Listen on PORT: %d\n", port_num);
-	int lis = listen(sockfd_server, 5);
-	if(lis < 0)
+	printf("Mode: %d\n",mode);
+	
+	if(mode == 1)
 	{
-		perror("Can't listen on this socket server");
-		return 0;
-	}
-	//return sockfd_server;
-	
-	//------------------
-	// send LIST
-	memset(buff, 0, sizeof buff);
-	recv(sockfd, buff, sizeof buff, 0);
-	printf("%s\n", buff);
-
-	printf("Listening\n");
-	len = sizeof(client_addr);
-	sockfd_client = accept(sockfd_server, (struct sockaddr*) &client_addr, &len);
-	
-	if(sockfd_client < 0)
-	{
-		perror("Accept error");
-		exit(1);
+		recv(sockfd, &size, sizeof(int), 0);
+		//char *buf = (char*)malloc(size + 1);
+		recv(sockfd, buf, size, 0);
+		printf("Server response: %s\n",buf);
+		//free(buf);
 	}
 
-	printf("Accepted!");
-	//------------------
-	
-	
-	//send(sock, buf, sizeof (buf), 0);
-	//Nhan data ve tu socket data moi mo hoi nay~
-	recv(sockfd_client, &size, sizeof(int), 0);
-	
+	recv(dataSock, &size, sizeof(int), 0);
 	file_buffer = (char*)malloc(size + 1);
-	recv(sockfd_client, file_buffer, size, 0);
+	memset(file_buffer, 0, size);
+	recv(dataSock, file_buffer, size, 0);
+	
 	
 	file_handler = creat(local_file, O_WRONLY);
 	write(file_handler, file_buffer, size);
 	close(file_handler);
 	printf("ls command result:\n\n");
-	system("cat ~temp.txt");
-
-	//return true;
+	printf("%s\n",file_buffer);
+	//system("cat ~ls.txt");
+	
+	return true;
 }
 
 bool command_handler(int sock, char* cmd)
@@ -550,169 +700,6 @@ bool command_handler(int sock, char* cmd)
 	return 1;
 }
 
-//Khoi tao data channel de gui nhan du lieu
-// Dung cho cac ham can goi lenh port len truoc roi moi nhan du lieu: get, put, ls
-
- int put(int sockfd, char filename[], char localPath[], int mode) // 0 active, 1 passive
- {
-	 char stor[BUFSIZE + 1];
-	 int command_code;
-	 //memset(stor, 0, sizeof(stor));
-	 if(strcmp(current_dir, "/") != 0)
-		 sprintf(stor, "STOR %s/%s\n", current_dir, filename);
-	 else
-		 sprintf(stor, "STOR %s%s\n", current_dir, filename);
-
-	char local[PATH_MAX + 1];
-	sprintf(local, "%s/%s", localPath, filename);
-	
-	FILE *f = fopen(local, "rt");
-	
-	if(f == NULL)
-	{
-		printf("Open file error!\n");
-		return 0;
-	}
-
-	printf("\nOpen file: %s\n", local);
-	 if(mode == 0) // active, server use port 20 for data transmission
-	 {
-		 // create new server address to connect
-		 char port[BUFSIZE];
-		 char buff[BUFSIZE];
-		 int min_port = 49153;
-		 int max_port = 65535;
-		struct sockaddr_in serv_addr, client_addr;
-		int sockfd_client, sockfd_server, len, nb, fd, i;
-		//struct hostent *host;
-
-		sockfd_server = socket(AF_INET, SOCK_STREAM, 0);
-		if(sockfd_server < 0)
-		{
-			perror("Socket can't be created!\n");
-			fclose(f);
-			return 0;
-		}
-
-		//serv_addr.sin_port = htons(20);
-		//inet_aton("192.168.127.2", &serv_addr.sin_addr.s_addr);
-		bzero((char*) &serv_addr, sizeof(serv_addr));
-		serv_addr.sin_family = AF_INET;
-		//serv_addr.sin_addr.s_addr = INADDR_ANY;
-		serv_addr.sin_addr.s_addr = inet_addr(myIP);
-		// memset(&serv_addr.sin_zero, 0, 8);
-
-		len = sizeof(client_addr);
-		int port_num=3425;
-
-		for( i = min_port; i <= max_port; i++)
-		{
-			serv_addr.sin_port = htons(i);
-
-			if(bind(sockfd_server, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) >= 0)
-			{
-				printf("FTP client receive data port: %d\n", i);
-				port_num = i;
-				break;
-			}
-		}
-
-		memset(buff, 0, sizeof(buff));
-		// put interface ip address into buff.
-		sprintf(buff, "%s", myIP);
-		//sprintf(buff, "%s", inet_ntoa(serv_addr.sin_addr));
-
-		for (int j = 0; j <strlen(buff); j++)
-		{
-			if(buff[j] == '.')
-			{
-				buff[j] = ',';
-			}
-		}
-
-		int a = port_num/256;
-		int b = port_num - a*256;
-		sprintf(port, "PORT %s,%d,%d\r\n", buff, a, b);
-		printf("Send command: %s\n", port);
-		if(send(sockfd, port, strlen(port), 0) < 0)
-		{
-			perror("Send error!");
-			fclose(f);
-			return 0;
-		}
-		//memset(buff, 0, sizeof(buff));
-		if (recv(sockfd, buff, sizeof(buff), 0) < 0)
-		{
-			perror("Receive error!");
-			fclose(f);
-			return 0;
-		}
-		else
-			printf("Server Response: %s\n", buff);
-
-		sscanf(buff, "%d", &command_code);
-		if(command_code != 200)
-		{
-			parseCode(command_code);
-			return 0;
-		}
-
-		printf("Listen on PORT: %d\n", port_num);
-		int lis = listen(sockfd_server, 5);
-		if(lis < 0)
-		{
-			perror("Can't listen on this socket server");
-			fclose(f);
-			return 0;
-		}
-
-		// send STOR
-		if(send(sockfd, stor, strlen(stor), 0) < 0)
-		{
-			perror("Send STOR error");
-			fclose(f);
-			return 0;
-		}
-
-		memset(buff, 0, sizeof buff);
-		recv(sockfd, buff, sizeof buff, 0);
-		printf("%s\n", buff);
-
-		printf("Listening\n");
-		len = sizeof(client_addr);
-		sockfd_client = accept(sockfd_server, (struct sockaddr*) &client_addr, &len);
-		if(sockfd_client < 0)
-		{
-			perror("Accept error");
-			exit(1);
-		}
-
-		printf("Accepted!");
-		// send data
-		memset(buff, 0, sizeof(buff));
-		while( (nb = read(f, buff, BUFSIZE))  > 0)
-		{
-			if(send(sockfd_client, buff, nb, 0) < 0 )
-			{
-				perror("send data error");
-				exit(1);
-			}
-		}
-
-		close(sockfd_server);
-		close(sockfd_client);
-		sockfd_client =0;
-		sockfd_server=0;
-		printf("Transmission complete!\n");
-	 }
-	 else // passive, server use don't use port 20 for data transmission
-	 {
-
-	 }
-
-	 fclose(f);
-	 return 1;
- }
 
 void parseCode(int command_code)
 {
